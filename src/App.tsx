6e645419db111b10940d83eaaec98034f6266596
linuxmobile <bdiez19@gmail.com>
2023-04-16 18:14:47 -0300

    :art: improving code
    adding new features to app.tsx and todoform

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
+import { TaskProvider } from './context/taskContext'
 import {TodoForm} from './components/TodoForm'
 
 function App() {
 	return (
-		<div>
+		<TaskProvider>
       <h1>TodoList w/ Typescript, React & Tailwindcss</h1>
       <TodoForm />
-		</div>
+		</TaskProvider>
 	)
 }
 
--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,13 +1,50 @@
 import {Input} from './Input'
+import UseLocalStorage from '../hooks/useLocalStorage'
+import { useDoneTask, useTodoTask } from '../context/taskContext'
 
-export const TodoForm = () => {
+const generatePlaceholder = (todoCount: number, doneCount: number) => {
+  if (doneCount > 7 && todoCount === 0) return 'great job!'
+  if (doneCount > 4 && todoCount < 3) return 'keep going!'
+  if (doneCount > 1 && todoCount < 5) return 'can be better!'
+  if (todoCount > 9) return "i don't even care anymore"
+  if (todoCount > 6) return 'ok, keep going...'
+  if (todoCount > 4) return 'are you sure?'
+  if (todoCount > 2) return 'more tasks?'
+  return 'ok, add some task here...'
+}
+
+type TodoFormProps = {
+  onSubmit: (description: string) => void
+}
+
+export function TodoForm (props: TodoFormProps) {
+  const { onSubmit } = props
+  const [input, setInput] = UseLocalStorage('ReactTodo:input', '')
+  const todo = useTodoTask()
+  const done = useDoneTask()
+  const todoLength = todo.tasks ? todo.tasks.length : 0
+  const doneLength = done.tasks ? done.tasks.length : 0
+  const placeholder = generatePlaceholder(todoLength, doneLength)
+
+  const handleInputChange = (description: string) => {
+    setInput(description)
+  }
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>, description: string) => {
+    e.preventDefault()
+    onSubmit(description)
+    setInput('')
+  }
   return (
-    <form>
+    <form onSubmit={(e) => handleSubmit(e, input)}> 
+      <label htmlFor='newTask'>New Task</label>
       <Input 
         id='newTask'
         name='newTask'
         type='text'
-        placeholder='Create TodoContext app...'
+        value={input}
+        placeholder={placeholder}
+        onChange={(e) => handleInputChange(e.currentTarget.value)}
       />
     </form>
   )
